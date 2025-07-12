2828cbcabaf971577bddc2f8eaac8e47c05e8e52
Harxsh-web <hk3631311@gmail.com>
2025-07-12 15:26:37 +0530

    configuration

--- src/pages/JewellStore/HomePage.tsx/HomePage.tsx
+++ src/pages/JewellStore/HomePage.tsx/HomePage.tsx
@@ -1,9 +1,12 @@
 import { NavbarDemo } from "../Navabar/Navbar"
 
 export const HomePage = () => {
-  return (
-    <div>
-        <NavbarDemo/>
-        HomePage</div>
-  )
+    return (
+        <>
+            <NavbarDemo />
+            <div>
+                HomePage
+            </div>
+        </>
+    )
 }
